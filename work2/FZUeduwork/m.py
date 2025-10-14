2a4f237bf6eeb5eb1174ebc3fcf5df53b3d2a710
rechenz <13244571368@qq.com>
2025-10-14 21:01:37 +0800

    2025年10月14日 21:01:41 work1暂时编写完成等待测试

--- work2/FZUeduwork/m.py
+++ work2/FZUeduwork/m.py
@@ -2,16 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import re
-import lxml
 url = "https://jwch.fzu.edu.cn/jxtz.htm"
 response = requests.get(url)
 response.encoding = 'utf-8'
 content = response.text
 soup = BeautifulSoup(content, 'lxml')
-li_find = soup.find_all('li')
-with open('fzu.csv', 'w', newline='', encoding='utf-8') as f:
+check = soup.find('ul', attrs={"class": "list-gl"})
+li_find = None
+if check != None:
+    li_find = check.find_all_next('li')
+else:
+    print("error")
+with open('AIsolution/work2/FZUeduwork/fzu.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerow(['通知人', '标题', '日期', '链接'])
+    if li_find == None:
+        exit()
     for li in li_find:
         annoucer = re.search("【(.*?)】", li.text)
         if annoucer:
@@ -28,4 +34,45 @@ with open('fzu.csv', 'w', newline='', encoding='utf-8') as f:
         temp = li.find('span')
         if temp:
             date = temp.string
+            if date == None:
+                tt = temp.find('font')
+                if tt:
+                    date = tt.string
         writer.writerow([annoucer, title, date, href])
+    for page in range(1, 206, 1):
+        url = f"https://jwch.fzu.edu.cn/jxtz/{206-page}.htm"
+        # print(url)
+        # writer.writerow([1, 1, 1, 1, 1])
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        content = response.text
+        soup = BeautifulSoup(content, 'lxml')
+        check = soup.find('ul', attrs={"class": "list-gl"})
+        li_find = None
+        if check != None:
+            li_find = check.find_all_next('li')
+        else:
+            print("error in for")
+        if li_find == None:
+            exit()
+        for li in li_find:
+            annoucer = re.search("【(.*?)】", li.text)
+            if annoucer:
+                annoucer = annoucer.group(1)
+            else:
+                annoucer = ""
+            temp = li.find('a')
+            title = None
+            date = None
+            href = None
+            if temp:
+                title = temp.string
+                href = temp.get('href')
+            temp = li.find('span')
+            if temp:
+                date = temp.string
+                if date == None:
+                    tt = temp.find('font')
+                    if tt:
+                        date = tt.string
+            writer.writerow([annoucer, title, date, href])
