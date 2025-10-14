a6ec92a45aff653d1dea3236e3c1f6d751619da8
rechenz <13244571368@qq.com>
2025-10-14 20:23:24 +0800

    2025年10月14日 20:23:27 爬虫第一次测试

--- work2/FZUeduwork/m.py
+++ work2/FZUeduwork/m.py
@@ -1,5 +1,31 @@
 import requests
-
+from bs4 import BeautifulSoup
+import csv
+import re
+import lxml
 url = "https://jwch.fzu.edu.cn/jxtz.htm"
 response = requests.get(url)
-print(response)
+response.encoding = 'utf-8'
+content = response.text
+soup = BeautifulSoup(content, 'lxml')
+li_find = soup.find_all('li')
+with open('fzu.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['通知人', '标题', '日期', '链接'])
+    for li in li_find:
+        annoucer = re.search("【(.*?)】", li.text)
+        if annoucer:
+            annoucer = annoucer.group(1)
+        else:
+            annoucer = ""
+        temp = li.find('a')
+        title = None
+        date = None
+        href = None
+        if temp:
+            title = temp.string
+            href = temp.get('href')
+        temp = li.find('span')
+        if temp:
+            date = temp.string
+        writer.writerow([annoucer, title, date, href])
