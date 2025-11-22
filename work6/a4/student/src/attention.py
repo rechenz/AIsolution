e8ca86320ed625d28d50a62b5304576d340c94f9
rechenz <13244571368@qq.com>
2025-11-22 15:32:15 +0800

    2025年11月22日 15:32:39 暂存

--- work6/a4/student/src/attention.py
+++ work6/a4/student/src/attention.py
@@ -62,6 +62,7 @@ def apply_rotary_emb(x, rope_cache):
     ### END YOUR CODE ###
     return rotated_x
 
+
 class CausalSelfAttention(nn.Module):
     """
     A vanilla multi-head masked self-attention layer with a projection at the end.
@@ -98,16 +99,19 @@ class CausalSelfAttention(nn.Module):
         self.proj = nn.Linear(config.n_embd, config.n_embd)
         # causal mask to ensure that attention is only applied to the left in the input sequence
         self.register_buffer("mask", torch.tril(torch.ones(config.block_size, config.block_size))
-                                     .view(1, 1, config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
         self.n_head = config.n_head
 
     def forward(self, x):
         B, T, C = x.size()
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        k = self.key(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = self.query(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = self.value(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        k = self.key(x).view(B, T, self.n_head, C //
+                             self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        q = self.query(x).view(B, T, self.n_head, C //
+                               self.n_head).transpose(1, 2)  # (B, nh, T, hs)
+        v = self.value(x).view(B, T, self.n_head, C //
+                               self.n_head).transpose(1, 2)  # (B, nh, T, hs)
 
         if self.rope:
             # TODO: [part g] Apply RoPE to the query and key.
@@ -118,11 +122,12 @@ class CausalSelfAttention(nn.Module):
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
 
-        att = att.masked_fill(self.mask[:,:,:T,:T] == 0, -1e10)
+        att = att.masked_fill(self.mask[:, :, :T, :T] == 0, -1e10)
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
-        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
 
         # output projection
         y = self.resid_drop(self.proj(y))
@@ -153,7 +158,7 @@ class CausalCrossAttention(nn.Module):
         self.proj = nn.Linear(config.n_embd, config.n_embd)
         # causal mask to ensure that attention is only applied to the left in the input sequence
         self.register_buffer("mask", torch.tril(torch.ones(config.block_size, config.block_size))
-                                     .view(1, 1, config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
         self.n_head = config.n_head
 
     def forward(self, x_kv, x_q):
@@ -161,26 +166,32 @@ class CausalCrossAttention(nn.Module):
         Bq, Tq, Cq = x_q.size()
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        
+
         # keys of x1
-        k = self.key(x_kv).view(Bk, Tk, self.n_head, Ck // self.n_head).transpose(1, 2) # (B, nh, Tk, hs)
-        
+        k = self.key(x_kv).view(Bk, Tk, self.n_head, Ck //
+                                self.n_head).transpose(1, 2)  # (B, nh, Tk, hs)
+
         # query with x2
-        q = self.query(x_q).view(Bq, Tq, self.n_head, Cq // self.n_head).transpose(1, 2) # (B, nh, Tq, hs)
-        
+        q = self.query(x_q).view(Bq, Tq, self.n_head, Cq //
+                                 # (B, nh, Tq, hs)
+                                 self.n_head).transpose(1, 2)
+
         # values from x1
-        v = self.value(x_kv).view(Bk, Tk, self.n_head, Ck // self.n_head).transpose(1, 2) # (B, nh, Tk, hs)
+        v = self.value(x_kv).view(Bk, Tk, self.n_head, Ck //
+                                  # (B, nh, Tk, hs)
+                                  self.n_head).transpose(1, 2)
 
         # causal self-attention;  (B, nh, Tk, hs) x (B, nh, hs, Tq) -> (B, nh, Tq, Tk)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        
+
         B = max(Bk, Bq)
-        
-        att = att.masked_fill(self.mask[:,:,:Tq,:Tk] == 0, -1e10) 
+
+        att = att.masked_fill(self.mask[:, :, :Tq, :Tk] == 0, -1e10)
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
-        y = att @ v # (B, nh, Tq, Tk) x (B, nh, Tk, hs) -> (B, nh, Tq, hs)
-        y = y.transpose(1, 2).contiguous().view(B, Tq, Cq) # re-assemble all head outputs side by side
+        y = att @ v  # (B, nh, Tq, Tk) x (B, nh, Tk, hs) -> (B, nh, Tq, hs)
+        # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).contiguous().view(B, Tq, Cq)
 
         # output projection
         y = self.resid_drop(self.proj(y))
