139db68be962ae0e9ff6d58a6ddd3fe0ed713033
rechenz <13244571368@qq.com>
2026-01-15 21:59:04 +0800

    feat:完成A1

--- work6/a4/student/scripts/tempCodeRunnerFile.bat
+++ work6/a4/student/scripts/tempCodeRunnerFile.bat
@@ -1,2 +1,1 @@
-
-@REM         --writing_params_path rope.pretrain.params
+python src\run.py evaluate rope wiki.txt --reading_params_path rope.finetune.params --eval_corpus_path birth_test_inputs.tsv --outputs_path rope.pretrain.test.predictions
